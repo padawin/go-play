49381a42ac0e213fa340374389645d2bb6af5292
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-03-21 09:02:41 +0000

    oooops

--- padawin/if.go
+++ padawin/if.go
@@ -8,7 +8,6 @@ func main() {
 	r1 := rand.New(s1)
 	fmt.Println("Find my number between 0 and 100, you have 10 turns!")
 	var solution int = r1.Intn(100)
-	fmt.Println(solution)
 	reader := bufio.NewReader(os.Stdin)
 	for turn := 0; turn < turns; turn += 1 {
 		fmt.Print("Enter text: ")
